c9d6e3824b77828e7f81c7366285ce45f3ffe246
Your Name <your-email@example.com>
2023-05-12 20:43:48 +0800

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
-
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { Box } from '@mui/material'
+import {Feed,Navbar,VideoDetail,SearchFeed,ChannelDetail} from "./components"
 function App() {
-
-  return (
-    <>
-    <h1>welcome to youtube clone app</h1>
+    return <>
+    <BrowserRouter>
+        <Box sx={{backgroundColor: "#000"}}>
+            <Navbar/>
+            <Routes>
+                <Route path='/' exact element={<Feed />}></Route>
+                <Route path='/video/:id' exact element={<VideoDetail />}></Route>
+                <Route path='/channel/:id' exact element={<ChannelDetail />}></Route>
+                <Route path='/search/:searchTerm' exact element={<SearchFeed />}></Route>
+            </Routes>
+        </Box>
+    </BrowserRouter>
     </>
-  )
 }
 
 export default App
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Paper, IconButton } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { Search } from '@mui/icons-material';
+function SearchBar() {
+  return (
+    <Paper 
+    component="form"
+    onSubmit={(()=>{})}
+    sx={{borderRadius:20, border: "1px solid #e3e3e3",pl:2,boxShadow:"none", mr:{ sm:5}}}
+    >
+        <input type="text" className='search-bar' placeholder='Search...' onChange={()=>{}} />
+        <IconButton type='submit' sx={{p:"10px",color:"red"}}>
+          <Search/>
+        </IconButton>
+    </Paper>
+  )
+}
+
+export default SearchBar
